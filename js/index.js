6bcb84be82e9298db536d7390b6939501732f437
Cristian Mansilla <cristianezequielmansilla@hotmail.com>
2022-11-07 08:46:45 -0300

    :sparkles: Feat: Add functions to print cards dynamically

---
+++ js/index.js
@@ -0,0 +1,186 @@
+const data={
+    "fechaActual": "2022-01-01",
+    "eventos": [
+        {
+            "image": "../assets/feria-comida.jpg",
+            "name":"Collectivities Party",
+            "date":"2021-12-12",
+            "description":"Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
+            "category":"Food Fair",
+            "place":"Room A",
+            "capacity":45000,
+            "assistance":42756,
+            "price":5
+        },
+        {
+            "image":"https://th.bing.com/th/id/R.df1d3a15771512d2d84c853bab30e393?rik=Ts0UROAzv5fwhQ&pid=ImgRaw&r=0",
+            "name":"Korean style",
+            "date":"2021-08-12",
+            "description":"Enjoy the best Korean dishes, with international chefs and awesome events.",
+            "category":"Food Fair",
+            "place":"Room A",
+            "capacity":45000,
+            "assistance":42756,
+            "price":10
+        },
+        {
+            "image":"../assets/museo.jpg",
+            "name":"Jurassic Park",
+            "date":"2021-11-02",
+            "description":"Let's go meet the biggest dinosaurs in the paleontology museum.",
+            "category":"Museum",
+            "place":"Field",
+            "capacity":82000,
+            "assistance":65892,
+            "price":15
+        },
+        {
+            "image":"https://th.bing.com/th/id/R.f9c6a269861ff61b42f44391bba9414f?rik=PxWXf2cCfNhm5w&riu=http%3a%2f%2fmy.tmyou.com%2fpublic%2fupload%2fimages%2f2019-01-19%2f20190119182611659188.jpg&ehk=3nFSgFCR3s%2fiGp%2bF8CBtTAmrFdWhNtUB5WU9bA8k97Q%3d&risl=&pid=ImgRaw&r=0",
+            "name":"Parisian Museum",
+            "date":"2022-11-02",
+            "description":"A unique tour in the city of lights, get to know one of the most iconic places.",
+            "category":"Museum",
+            "place":"Paris",
+            "capacity":8200,
+            "estimate":8200,
+            "price":3500
+        },
+        {
+            "image":"../assets/disfraces.jpg",
+            "name":"Comicon",
+            "date":"2021-02-12",
+            "description":"For comic lovers, all your favourite characters gathered in one place.",
+            "category":"Costume Party",
+            "place":"Room C",
+            "capacity":120000,
+            "assistance":110000,
+            "price":54
+        },
+        {
+            "image":"https://th.bing.com/th/id/OIP.Wq2-vJf_URvNDzNpZEX0lQHaE0?pid=ImgDet&rs=1",
+            "name":"Halloween Night",
+            "date":"2022-02-12",
+            "description":"Come with your scariest costume and win incredible prizes.",
+            "category":"Costume Party",
+            "place":"Room C",
+            "capacity":12000,
+            "estimate":9000,
+            "price":12
+        },
+        {
+            "image":"https://th.bing.com/th/id/R.6f7ba5fed07f3fddae5cf86eeb2a34dd?rik=jF70e5FgvmVU%2bw&pid=ImgRaw&r=0",
+            "name":"Metallica in concert",
+            "date":"2022-01-22",
+            "description":"The only concert of the most emblematic band in the world.",
+            "category":"Music Concert",
+            "place":"Room A"
+            ,"capacity":138000,
+            "estimate":138000,
+            "price":150
+        },
+        {
+            "image":"https://th.bing.com/th/id/R.08b69914b198c2d2c16b227d8333236f?rik=sYclTnszOcVrAg&pid=ImgRaw&r=0",
+            "name":"Electronic Fest",
+            "date":"2021-01-22",
+            "description":"The best national and international DJs gathered in one place.",
+            "category":"Music Concert",
+            "place":"Room A",
+            "capacity":138000,
+            "assistance":110300,
+            "price":250
+        },
+        {
+            "image":"../assets/maraton.jpg",
+            "name":"10K for life",
+            "date":"2021-03-01",
+            "description":"Come and exercise, improve your health and lifestyle.",
+            "category":"Race",
+            "place":"Campo de FutbÃ³l",
+            "capacity":30000,
+            "assistance":25698,
+            "price":3
+        },
+        {
+            "image":"https://th.bing.com/th/id/R.4155e1f73c920c3ba96333159fb484ba?rik=W0DA0cRGRr%2fqvg&pid=ImgRaw&r=0",
+            "name":"15K NY",
+            "date":"2021-03-01",
+            "description":"We'll be raising funds for hospitals and medical care in this unique event held in The Big Apple.",
+            "category":"Race",
+            "place":"New York",
+            "capacity":3000000,
+            "assistance":2569800,
+            "price":3
+        },
+        {
+            "image":"https://th.bing.com/th/id/R.b889bd24c07683f6b599e0b19bbe12e5?rik=sBweLOsqdLwYMA&pid=ImgRaw&r=0",
+            "name":"School's book fair",
+            "date":"2022-10-15",
+            "description":"Bring your unused school book and take the one you need.",
+            "category":"Book Exchange",
+            "place":"Room D1",
+            "capacity":150000,
+            "estimate":123286,
+            "price":1
+        },
+        {
+            "image":"https://th.bing.com/th/id/R.9345e047881445954872322520ec7b90?rik=APQG401jcS5dQw&riu=http%3a%2f%2fmexiconuevaera.com%2fsites%2fdefault%2ffiles%2fstyles%2finterior_noticia%2fpublic%2fimagenes%2f2016%2fOct%2f14%2fdsc06901.jpg%3fitok%3dlIMUh3ea&ehk=Fch2JYbyOqULhY%2f60iAqck%2bfI%2bT8vwzdNzaE19QrmcY%3d&risl=&pid=ImgRaw&r=0",
+            "name":"Just for your kitchen",
+            "date":"2021-11-09",
+            "description":"If you're a gastronomy lover come get the cookbook that best suits your taste and your family's.",
+            "category":"Book Exchange",
+            "place":"Room D6",
+            "capacity":130000,
+            "assistance":90000,
+            "price":100
+        },
+        {
+            "image":"https://files.venuu.se/attachments/000/033/535/a2dea3fb6bd06f45cbdde5c26d82ff9d820feed3.jpg",
+            "name":"Batman",
+            "date":"2021-3-11",
+            "description":"Come see Batman fight crime in Gotham City.",
+            "category":"Cinema",
+            "place":"Room D1",
+            "capacity":11000,
+            "assistance":9300,
+            "price":225
+        },
+        {
+            "image":"../assets/cine.jpg",
+            "name":"Avengers",
+            "date":"2022-10-15",
+            "description":"Marvel's Avengers Premier in 3d, the start of an epic saga with your favourite superheroes.",
+            "category":"Cinema",
+            "place":"Room D1",
+            "capacity":9000,
+            "estimate":9000,
+            "price":250
+        }
+    ]
+}
+const $cardsContainer = document.getElementById("cards-container");
+
+function paintDom(dateDom){
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
+    <div class="card h-210">
+        <img src="${dateDom.image}" class="card-img-top" alt="">
+        <div class="card-body d-flex flex-column align-items-center">
+            <div>
+                <h5 class="card-title">${dateDom.name}</h5>
+                <p class="card-text">${dateDom.description}</p>
+            </div>
+            <div class="price d-flex justify-content-between align-items-start">
+                <p>Price $${dateDom.price}</p>
+                <a id="seeMore" href="./pages/details.html"><button class="button">See more</button></a>
+            </div>
+        </div>
+    </div>
+    `
+    $cardsContainer.appendChild(div);
+};
+
+data.eventos.forEach((event)=>{
+    paintDom(event)
+})
+
